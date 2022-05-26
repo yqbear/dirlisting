2c7e2641f436a224628b2510dc19670c914c6ede
Stephan Poole (e122785) <stephan.poole@honeywell.com>
2022-05-25 21:12:15 -0700

    feat: add CLI
    name of app is `dirlisting`

---
+++ src/dirlisting/cli.py
@@ -0,0 +1,27 @@
+import click
+
+from dirlisting.dirlisting import Dirlisting
+
+
+@click.command()
+@click.argument('file', type=click.File())
+def app(file):
+    """Create a directory listing given an input FILE.
+
+    The input should be a yaml file with directories having a colon
+    after them and files without.
+
+    Example
+
+    \b
+    - topdir:
+      - file1.txt
+      - somedir:
+        - another_file.txt
+    """
+    listing = Dirlisting(file)
+    listing.print()
+
+
+if __name__ == "__main__":
+    app()
