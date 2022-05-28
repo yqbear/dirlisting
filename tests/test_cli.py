ec236743a53f9a9c395847ecf10ec36412690259
Stephan Poole <yqbear@gmail.com>
2022-05-28 09:50:48 -0700

    test: add tests for version and sort options

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,14 @@ from inspect import cleandoc
 from click.testing import CliRunner
 from dirlisting.cli import app
 
+from test_helper import assert_by_lines
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(app, ["--version"])
+    assert "app, version" in result.output
+
 
 def test_file_input():
     input = cleandoc(
@@ -22,3 +30,33 @@ def test_file_input():
 
         result = runner.invoke(app, ["input.yaml"])
         assert "toplevel" in result.output
+
+
+def test_sorted():
+    input = cleandoc(
+        """
+        - toplevel:
+          - xdir:
+            - bfile.txt
+            - afile.txt
+          - mdir:
+            - cfile.txt
+            - dfile.txt
+        """
+    )
+    expected = """
+        toplevel
+        ├── mdir
+        │   ├── cfile.txt
+        │   └── dfile.txt
+        └── xdir
+            ├── afile.txt
+            └── bfile.txt
+        """
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("input.yaml", "w") as f:
+            f.write(input)
+
+        result = runner.invoke(app, ["--sort", "input.yaml"])
+        assert_by_lines(result.output, expected)
