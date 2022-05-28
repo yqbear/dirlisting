adc294ad22f2192e55381e1e09870a07dc044d23
Stephan Poole <yqbear@gmail.com>
2022-05-28 14:47:04 -0700

    test: add tests for dirsfirst option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 from inspect import cleandoc
 
+import pytest
 from click.testing import CliRunner
 from dirlisting.cli import app
 
@@ -9,6 +10,7 @@ from test_helper import assert_by_lines
 def test_version():
     runner = CliRunner()
     result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
     assert "app, version" in result.output
 
 
@@ -29,26 +31,36 @@ def test_file_input():
             f.write(input)
 
         result = runner.invoke(app, ["input.yaml"])
+        assert result.exit_code == 0
         assert "toplevel" in result.output
 
 
-def test_sorted():
+@pytest.fixture
+def unsorted():
     input = cleandoc(
         """
         - toplevel:
           - xdir:
             - bfile.txt
             - afile.txt
+          - pfile.txt
           - mdir:
             - cfile.txt
             - dfile.txt
+          - efile.txt
         """
     )
+    return input
+
+
+def test_sorted(unsorted):
     expected = """
         toplevel
+        ├── efile.txt
         ├── mdir
         │   ├── cfile.txt
         │   └── dfile.txt
+        ├── pfile.txt
         └── xdir
             ├── afile.txt
             └── bfile.txt
@@ -56,7 +68,30 @@ def test_sorted():
     runner = CliRunner()
     with runner.isolated_filesystem():
         with open("input.yaml", "w") as f:
-            f.write(input)
+            f.write(unsorted)
 
         result = runner.invoke(app, ["--sort", "input.yaml"])
+        assert result.exit_code == 0
+        assert_by_lines(result.output, expected)
+
+
+def test_dirsfirst(unsorted):
+    expected = """
+        toplevel
+        ├── xdir
+        │   ├── bfile.txt
+        │   └── afile.txt
+        ├── mdir
+        │   ├── cfile.txt
+        │   └── dfile.txt
+        ├── pfile.txt
+        └── efile.txt
+        """
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("input.yaml", "w") as f:
+            f.write(unsorted)
+
+        result = runner.invoke(app, ["--dirsfirst", "input.yaml"])
+        assert result.exit_code == 0
         assert_by_lines(result.output, expected)
